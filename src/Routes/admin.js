41fe3ffc96c6828f4c383da1791a7a59a76c8136
J31S0N <jeisonp33@gmail.com>
2023-11-17 16:22:23 -0500

    mi segundo commit

---
+++ src/Routes/admin.js
@@ -0,0 +1,10 @@
+function Admin() {
+    return <section class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Bienvenido, Administrador</h2>
+        </div>
+    </div>
+</section>;
+}
+export default Admin;
---
+++ src/Routes/logup.js
@@ -0,0 +1,8 @@
+import NavBar from "../Components/navBar";
+function Logup() {
+return (
+    <>
+<NavBar></NavBar>
+    </>);
+}
+export default Logup;
